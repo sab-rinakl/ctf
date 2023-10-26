7b945e7784c707d326dd7d4f77e2348f7292cd1d
Sabrina Lawrence <sklawren@usc.edu>
2023-10-25 17:03:29 -0700

    Update replay.py
    update packet sending, still need to find the packet

--- replay.py
+++ replay.py
@@ -3,19 +3,24 @@ from scapy.all import *
 target_ip = "blueserver"  
 target_port = 80
 
-# Dummy packet data that we're 'replaying'
 packet_data = "This is the packet data we intercepted and are re-sending."
 
 def send_packet(packet_content):
-    # Craft the packet with Scapy
-    packet = IP(dst=target_ip)/TCP(dport=target_port)/Raw(load=packet_content)
-    # Send the packet
-    send(packet)
+    packet = IP(dst=target_ip)/TCP(dport=target_port, flags='S')/Raw(load=packet_content)
+    
+    response = sr1(packet, timeout=2)
+    if response:
+        print("Received response from target.")
+    else:
+        print("No response from target.")
 
 def main():
     print(f"Sending intercepted packet data to {target_ip}:{target_port}")
-    send_packet(packet_data)
-    print("Packet sent.")
+    try:
+        send_packet(packet_data)
+        print("Packet sent.")
+    except Exception as e:
+        print(f"Error sending packet: {e}")
 
 if __name__ == "__main__":
     main()
